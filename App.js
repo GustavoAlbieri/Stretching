bd567ef316d7d2c47041a1a5fc4ba4b0030475de
Gustavo Albieri <gustavo.albieri1999@gmail.com>
2025-02-04 23:12:36 -0300

    Commit before EAS build

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { AgeProvider } from "./src/context/AgeContext";
+import AppTabs from "./src/navigation/AppTabs"; // Substitua pelo seu componente de navegação
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AgeProvider>
+      <NavigationContainer>
+        <AppTabs />
+      </NavigationContainer>
+    </AgeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/dataBase/stretches.js
@@ -0,0 +1,53 @@
+const stretches = {
+  "0-18": [
+    {
+      name: "Alongamento de Pernas",
+      description: "Alongamento para melhorar a flexibilidade das pernas.",
+      image: require("../assets/alongamento-pernas.jpg"),
+    },
+    {
+      name: "Alongamento de Braços",
+      description: "Alongamento focado em aumentar a flexibilidade dos braços.",
+      image: require("../assets/alongamento-braco.jpg"),
+    },
+  ],
+  "19-35": [
+    {
+      name: "Alongamento de Costas",
+      description: "Alongamento para aliviar tensões nas costas.",
+      image: require("../assets/alongamento-costas.jpg"),
+    },
+    {
+      name: "Alongamento de Pescoço",
+      description: "Alongamento para melhorar a mobilidade do pescoço.",
+      image: require("../assets/alongamento-pescoco.jpg"),
+    },
+  ],
+  "36-60": [
+    {
+      name: "Alongamento de Quadril",
+      description: "Alongamento para melhorar a flexibilidade do quadril.",
+      image: require("../assets/alongamento-quadril.jpg"),
+    },
+    {
+      name: "Alongamento de Ombros",
+      description: "Alongamento para aliviar tensões nos ombros.",
+      image: require("../assets/alongamento-ombros.jpg"),
+    },
+  ],
+  "61+": [
+    {
+      name: "Alongamento de Coluna",
+      description: "Alongamento suave para a coluna, ideal para idosos.",
+      image: require("../assets/alongamento-coluna.jpg"),
+    },
+    {
+      name: "Alongamento de Panturrilha",
+      description:
+        "Alongamento para melhorar a flexibilidade das panturrilhas.",
+      image: require("../assets/alongamento-panturrilha.jpg"),
+    },
+  ],
+};
+
+export default stretches;
